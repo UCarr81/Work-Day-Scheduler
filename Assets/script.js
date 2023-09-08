77d7d3e6dfc4ffbb5fde92ed1b1b96a915a6ab4b
Uriel <CarrilloUriel81@gmail.com>
2023-09-07 19:31:22 -0700

    added localstorage to each hour blcok tweaked a few problems

--- Assets/script.js
+++ Assets/script.js
@@ -22,8 +22,38 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
 
-  var currentDate = dayjs().format("dddd, MMMM D, YYYY");
+  $("#hour09 .description").val(localStorage.getItem("hour09"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour13 .description").val(localStorage.getItem("hour13"));
+  $("#hour14 .description").val(localStorage.getItem("hour14"));
+  $("#hour15 .description").val(localStorage.getItem("hour15"));
+  $("#hour16 .description").val(localStorage.getItem("hour16"));
+  $("#hour17 .description").val(localStorage.getItem("hour17"));
+  
+
+
+  function updateBlockClasses() {
+    // Get the current hour using Day.js and convert it to a number
+    var currentHour = dayjs().hour();
+  
+    // Iterate through time blocks and apply classes based on their time
+    $(".time-block").each(function () {
+      var hourBlock = parseInt($(this).attr("id").split("hour")[1]);
+      console.log(hourBlock, currentHour);
+      if (hourBlock < currentHour) {
+        $(this).removeClass("present future").addClass("past");
+      } else if (hourBlock === currentHour) {
+        $(this).removeClass("past future").addClass("present");
+      } else {
+        $(this).removeClass("past present").addClass("future");
+      }
+    });
+  }
+  updateBlockClasses();
+
+  var currentDate = dayjs().format("dddd, MMMM D, YYYY, h:mm:ss a");
   $("#currentDay").text(currentDate);
 });
